654423160b2efa2ed0bf63d6bb211b98d61f6aa6
Paules <pauvz123@gmail.com>
2024-05-01 15:25:28 +0200

    nearly nearly nearly finished

--- Unity Scene/Assets/Scripts/FootstepSound.cs
+++ Unity Scene/Assets/Scripts/FootstepSound.cs
@@ -8,39 +8,69 @@ public class FootstepSound : MonoBehaviour
     public AudioClip[] footstepsOnWood;
 
     public string material;
+    private int TimeToStep = 40;
+    private int timer = 0;
 
-    void PlayFootstepSound()
+    void Update()
     {
-        AudioSource myAudioSource = GetComponent<AudioSource>();
-        myAudioSource.volume = Random.Range(0.9f, 1.0f);
-        myAudioSource.pitch = Random.Range(0.8f, 1.2f);
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
 
-        switch (material)
+        if (vAxis > 0.1 || vAxis < -0.1)
         {
+            if (timer > TimeToStep)
+            {
+                AudioSource myAudioSource = GetComponent<AudioSource>();
+                myAudioSource.volume = Random.Range(0.9f, 1.0f);
+                myAudioSource.pitch = Random.Range(0.8f, 1.2f);
+
+                switch (material)
+                {
+                    case "Grass":
+                        myAudioSource.PlayOneShot(footstepsOnGrass[Random.Range(0, footstepsOnGrass.Length)]);
+                        break;
+
+                    case "Wood":
+                        myAudioSource.PlayOneShot(footstepsOnWood[Random.Range(0, footstepsOnWood.Length)]);
+                        break;
+                    case "Jumping":
+                        break;
+                    default:
+                        break;
+                }
+                timer = 0;
+            }
+            timer++;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hello: adwa");
+        switch (collision.gameObject.tag)
+        {
+            
             case "Grass":
-                myAudioSource.PlayOneShot(footstepsOnGrass[Random.Range(0, footstepsOnGrass.Length)]);
+                material = collision.gameObject.tag;
                 break;
-
             case "Wood":
-                myAudioSource.PlayOneShot(footstepsOnWood[Random.Range(0, footstepsOnWood.Length)]);
+                material = collision.gameObject.tag;
                 break;
-
             default:
                 break;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("Hello: ");
         switch (collision.gameObject.tag)
         {
-            
             case "Grass":
-                Debug.Log("Hello: " + gameObject.tag);
+                material = "Jumping";
                 break;
             case "Wood":
-                material = collision.gameObject.tag;
+                material = "Jumping";
                 break;
 
             default:
